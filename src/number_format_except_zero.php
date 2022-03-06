0c76afd06c478166dc707a1665b09dda719fe04c
David van der Tuijn <david@digitalimpact.nl>
2022-03-06 23:17:05 +0100

    Code Optimization

--- src/number_format_except_zero.php
+++ src/number_format_except_zero.php
@@ -3,18 +3,18 @@
 /**
  * Number Format Except Zero.
  *
- * @param float $fNumber
- * @param int $iDecimals
- * @param string $sDecPoint
- * @param string $sThousandsSep
+ * @param float $num
+ * @param int $decimals
+ * @param string|null $decimal_separator
+ * @param string|null $thousands_separator
  *
  * @return string
  */
-function number_format_except_zero(float $fNumber, int $iDecimals, string $sDecPoint = '.', string $sThousandsSep = ',') : string
+function number_format_except_zero(float $num, int $decimals, ?string $decimal_separator = '.', ?string $thousands_separator = ','): string
 {
-    if ($fNumber == 0) {
+    if ($num == 0) {
         return '0';
     }
 
-    return number_format($fNumber, $iDecimals, $sDecPoint, $sThousandsSep);
+    return number_format($num, $decimals, $decimal_separator, $thousands_separator);
 }
